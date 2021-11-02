add98f1cd790016fab3e629d59ebbdea9a6e467a
ShaboJohnson <shabojohnson@gmail.com>
2021-11-02 14:14:53 -0500

    Prueba 2

---
+++ ConsoleApp1/Formato.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoMax
+{
+    class Formato
+    {
+        public int FormatoId { get; }
+
+        public string Description { get;  }
+
+        public int Precio { get;  }
+        
+
+        public Formato (int id, int precio, string description)
+        {
+            FormatoId = id;
+            Precio = precio;
+            Description = description;
+
+
+
+
+        }
+
+
+
+
+    }
+}
---
+++ ConsoleApp1/Genero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoMax
+{
+    class Genero
+    {
+        public int GeneroId { get; }
+
+        public string Description { get; }
+
+
+        public Genero (int generoId, string description)
+        {
+            GeneroId = generoId;
+            Description = description;
+
+
+
+        }
+
+
+
+    }
+}
